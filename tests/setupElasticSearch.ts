d0041939dbf1ada21a46e17b2da428796d5d64fb
Przemysław Żydek <przemyslaw.zydek@gmail.com>
2020-08-02 09:27:43 +0200

    test: set timeout

--- tests/setupElasticSearch.ts
+++ tests/setupElasticSearch.ts
@@ -6,11 +6,16 @@ beforeAll(async () => {
     node: 'http://localhost:9200',
   });
 
+  jest.setTimeout(900000);
+
   await new Promise((resolve) => {
     (function init() {
       global.esClient
         .ping()
-        .then(resolve)
+        .then(() => {
+          jest.setTimeout(9000);
+          resolve();
+        })
         .catch(() => {
           setTimeout(init, 2000);
         });

a31185e2bf72a5be34a33e784a43cecdebf25dc9
Przemysław Żydek <przemyslaw.zydek@gmail.com>
2020-08-02 09:24:34 +0200

    test: wait for client

--- tests/setupElasticSearch.ts
+++ tests/setupElasticSearch.ts
@@ -1,9 +1,19 @@
 import { Client } from '@elastic/elasticsearch';
 
+// TODO Retry
 beforeAll(async () => {
   global.esClient = new Client({
     node: 'http://localhost:9200',
   });
 
-  await global.esClient.ping();
+  await new Promise((resolve) => {
+    (function init() {
+      global.esClient
+        .ping()
+        .then(resolve)
+        .catch(() => {
+          setTimeout(init, 2000);
+        });
+    })();
+  });
 });
